435bf47ae3f68fb0b82ff568dfe1a4dba66f9848
victoriaterezabragacordeiro <135035969+victoriaterezabragacordeiro@users.noreply.github.com>
2024-08-14 19:49:44 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,169 @@
+export const perguntas = [
+    {
+        enunciado: "O que você acha de estar aprendendo sobre primeiros socorros através de uma tela?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao:[
+                    "Ficou com medo."
+                ],
+             
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao:[
+                    "Quis saber como iria funcionar."
+                ],
+               
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "O que deve ser feito primeiro ao encontrar uma pessoa desmaiada?",
+        alternativas: [
+            {
+                texto: "Verificar se a pessoa está respirando e tem pulso.",
+                afirmacao:[
+                    "Aqui percebemos que a pessoa sabe o basico."
+                ],
+               
+                proxima: 3,
+            },
+            {
+                texto: "Só ficar olhando.",
+                afirmacao:[
+                    "Aqui percebemos que a pessoa não tem coração."
+                ],
+               
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como deve ser realizada a manobra de Heimlich em uma criança que está engasgada?",
+        alternativas: [
+            {
+                texto: "Aplicar compressas quentes no pescoço da criança.",
+                afirmacao:[
+                "Tem que estudar mais."
+
+                ],
+               
+                proxima: 3,
+            },
+            {
+                texto: "Colocar os braços ao redor da cintura, posicionar um punho acima do umbigo e realizar compresões rápidas.",
+                afirmacao:[
+                    "Está indo pelo caminho certo."
+                ],
+               
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado:"O que deve ser feito se alguém estiver tendo uma convulção?",
+        alternativas: [
+            {
+                texto: "Garantir que o ambiente ao redor esteja seguro e monitorar a pessoa até que a convulção termine.",
+                afirmacao:[
+                    ""
+                ],
+               
+                proxima: 5,
+            },
+            {
+                texto: "Colocar um objeto na boca da pessoa para evitar que ela morda a língua.",
+                afirmacao:[
+                    ""
+                ],
+               
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o procedimento correto para lidar com uma hemorragia severa?",
+        alternativas: [
+            {
+                texto: "Aplicar uma bandagem limpa diretamente sobre a ferida e pressionar firmemente.",
+                afirmacao:[
+                    ""
+                ],
+               
+                proxima: 5,
+            },
+            {
+                texto: "Lavar com água fria.",
+                afirmacao:[
+                    ""
+                ],
+               
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como deve ser aplicada a posição lateral de segurança?",
+        alternativas: [
+            {
+                texto: "Manter a pessoa em pé até a ajuda chegar.",
+                afirmacao:[
+                    ""
+                ],
+               
+                proxima: 7,
+            },
+            {
+                texto: "Colocar a pessoa de lado, dobrar a perna superior e apoiar o braço superior sobre a perna.",
+                afirmacao:[
+                    ""
+                ],
+               
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "O que deve ser feito em caso de suspeita de fratura?",
+        alternativas: [
+            {
+                texto: "Dar analgésicos sem orientação médica.",
+                afirmacao:[
+                    ""
+                ],
+               
+                proxima: 7,
+            },
+            {
+                texto: "Imobilizar a área afetada e evitar movimentar a parte fraturada.",
+                afirmacao:[
+                    ""
+                ],
+               
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "O que você deve fazer se uma pessoa está com sintomas de choque (pele pálida, suor frio, pulso rápido)?",
+        alternativas: [
+            {
+                texto: "Forçá-la a comer algo doce.",
+                afirmacao:[
+                    ""
+                ]
+               
+            },
+            {
+                texto: "Manter a pessoa deitada, elevar as pernas e cobri-la para manter o calor.",
+                afirmacao:[
+                    ""
+                ]
+               
+            },
+        ]
+    }
+];
